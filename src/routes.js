e208c6267f0ad1a41e016cf3ca8885b6b4aced01
lucasark <lucas.arkantos@hotmail.com>
2019-09-21 15:05:31 -0300

    Implementação de rotas do portal

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+import Paciente from "./components/Portal/Paciente"
+import Home from "./components/Portal/Home"
+import Exame from "./components/Portal/Exame"
+import RealizarExame from "./components/Portal/RealizarExame"
+import Analise from "./components/Portal/Analise"
+import Resultados from "./components/Portal/Resultados"
+
+const routes = [
+    {
+        path: "/home",
+        name: "home",
+        component: Home,
+        layout: "/portal"
+    },
+    {
+        path: "/paciente",
+        name: "paciente",
+        component: Paciente,
+        layout: "/portal"
+    },
+    {
+        path: "/exame",
+        name: "exame",
+        component: Exame,
+        layout: "/portal"
+    },
+    {
+        path: "/RealizarExame",
+        name: "RealizarExame",
+        component: RealizarExame,
+        layout: "/portal"
+    },
+    {
+        path: "/Analise",
+        name: "Analise",
+        component: Analise,
+        layout: "/portal"
+    },
+    {
+        path: "/Resultados",
+        name: "Resultados",
+        component: Resultados,
+        layout: "/portal"
+    }
+    
+];
+export default routes;
