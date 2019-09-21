f7ed404d8eed9645ca6a9605a9b3e10a972e4336
lucasark <lucas.arkantos@hotmail.com>
2019-09-21 15:03:28 -0300

    Ajuste de roteamento principal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Menu from "./layouts/Menu";
+import Portal from "./layouts/Portal";
 
+//TODO 404 e Login Invalido
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/menu" render={ () => <Menu />}/>
+        <Route path="/menu" render={ props => <Menu {...props}/>}/>
+        <Route path="/portal" render={ props => <Portal {...props}/>}/> 
         <Redirect from="/" to="/menu/inicio" />
       </Switch>
     </BrowserRouter>
