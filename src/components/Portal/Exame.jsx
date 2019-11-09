a9fb29a2c27e901f9df14a1de88c79cefb2efad6
lucasark <lucas.arkantos@hotmail.com>
2019-11-09 16:41:58 -0300

    Implementação dos Componentes Juntos

--- src/components/Portal/Exame.jsx
+++ src/components/Portal/Exame.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 //import CadastroPaciente from '../CadastroPaciente/CadastroPaciente';
 //import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import ExameCadastro from "../Model/ExameCadastro"
 import ExameLista from "./ExameLista";
-import { Button } from "react-bootstrap";
 
 export default props => {
+
+    const [tela, setTela] = useState(1);
+    const [exame, setExame] = useState({});
+
+    const handleTela = () =>{
+        switch(tela){
+            case 1:
+                return(<ExameLista handle={setTela} exame={setExame}></ExameLista>)
+            case 2:
+                return(<ExameCadastro exame={exame}></ExameCadastro>)
+        }
+    }
     return (
         <div>
             <h2>Exames</h2>
-            <ExameLista></ExameLista>
+            {handleTela()}
         </div>
     );
 }
