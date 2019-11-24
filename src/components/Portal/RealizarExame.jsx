91bfffe59773858def254385aeb64e6a343c65d2
lucasark <lucas.arkantos@hotmail.com>
2019-11-24 15:49:51 -0300

    Handle da tela de Realizar Exame

--- src/components/Portal/RealizarExame.jsx
+++ src/components/Portal/RealizarExame.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import PreExame from '../Exame/PreExame';
+import PosExame from '../Exame/PosExame';
 
 
 export default props => {
+
+    const [tela, setTela] = useState(props.tela ? props.tela : 1);
+
+    const handleTela = () =>{
+        switch(tela){
+            case 1:
+                return(<PreExame></PreExame>)
+            case 2:
+                return(<PosExame></PosExame>)
+        }
+    }
+    
     return(
         <div>
             <h2>Novo Exame</h2>
-            <PreExame></PreExame>
-            
+            {handleTela()}
         </div>
     );
 }
