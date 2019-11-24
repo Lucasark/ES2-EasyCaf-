a9cbbba2b18968ddb0cef8748f900c741221efb8
lucasark <lucas.arkantos@hotmail.com>
2019-11-24 15:47:53 -0300

    Remoção de Codigo em CustomButton

--- src/components/Model/CustomButton.jsx
+++ src/components/Model/CustomButton.jsx
@@ -4,38 +4,10 @@ import cx from "classnames";
 import PropTypes from "prop-types";
 import "./CustomButton.css";
 
-// class CustomButton extends Component {
-//   render() {
-//     const { fill, simple, pullRight, round, block, ...rest } = this.props;
-
-//     const btnClasses = cx({
-//       "btn-fill": fill,
-//       "btn-simple": simple,
-//       "pull-right": pullRight,
-//       "btn-block": block,
-//       "btn-round": round
-//     });
-
-//     return <Button className="" {...rest} />;
-//   }
-// }
-
-// CustomButton.propTypes = {
-//   fill: PropTypes.bool,
-//   simple: PropTypes.bool,
-//   pullRight: PropTypes.bool,
-//   block: PropTypes.bool,
-//   round: PropTypes.bool
-// };
-
-// export default CustomButton;
-
-
-
 export default props => {
 
   return (
-    <button {...props} className={props.className+' botaoNovo'} style={props.style}>{props.btnText}</button>
+    <button {...props} className={props.className+' botaoNovo'} style={props.style}>{props.btntext}</button>
   );
 
 }
