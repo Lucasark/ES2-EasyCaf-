5140b0f4bb2f0d75f59ca4d4838e21ec2fbe756d
lucasark <lucas.arkantos@hotmail.com>
2019-11-24 15:48:31 -0300

    Acresentando o Buttom para continuar o Estresse

--- src/components/Model/ExameCadastro.jsx
+++ src/components/Model/ExameCadastro.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ButtonDefault from "../Model/CustomButton";
 
 export default props => {
     console.log(props.exame)
     const exame = props.exame;
+
     return(
-        <div class="container">
+        <div className="container">
+            <ButtonDefault className="col-md-2" btntext="Voltar" onClick={() => props.handle(1)}></ButtonDefault>
             <h3>Exame número :{props.exame.id}</h3>
             <div className="col-md-12">
                 <div className="col-md-4">
                     <p style={{fontVariant: 'all-petite-caps'}}>Nome:{exame.exame_paciente.nome}</p>
                 </div>
             </div>
+            <ButtonDefault className="col-md-2" btntext="Estresse" onClick={() => props.handle(3)}></ButtonDefault>
         </div>
     )
 }
